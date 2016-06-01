513fb43f383992743aa5c5bf3fb6f8e53a94923a
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-06-01 10:10:25 -0400

    adds create and delete functionality

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var mongoose = require("./data/connection");
-var hbs = require('express-handlebars')
+var hbs = require('express-handlebars');
+var parser = require('body-parser')
 
 var app = express();
 
 var Meditation = mongoose.model("Meditation");
 
+app.use(parser.json({extended: true}));
 app.use("/assets", express.static("public"));
 app.set("port", process.env.PORT || 4321)
 app.set("view engine", "hbs");
@@ -21,6 +23,24 @@ app.get("/api/meditations", function(req, res){
     res.json(meditations_db)
   })
 })
+
+app.post("/api/meditations", function(req, res){
+  Meditation.create(req.body).then(function(new_meditation){
+    res.json(new_meditation)
+  })
+})
+
+app.delete("/api/meditations/:_id", function(req, res){
+  Meditation.findOneAndRemove(req.params).then(function(){
+   res.json({success: true});
+ });
+});
+
+app.put("/api/meditations/:_id", function(req, res){
+  Meditation.findOneAndUpdate(req.params, req.body, {new: true}).then(function(meditation){
+    res.json(meditation);
+  });
+});
 
 app.get("/*", function(req, res){
   res.render("main", {layout:false})
--- public/app.js
+++ public/app.js
@@ -10,9 +10,8 @@
   .controller("Index", IndexCtrl)
   .factory("Meditation", MeditationFactory)
 
-
-Router.$inject = ["$stateProvider", "$locationProvider"];
-function Router($stateProvider, $locationProvider){
+Router.$inject = ["$stateProvider", "$locationProvider",  "$urlRouterProvider"];
+function Router($stateProvider, $locationProvider, $urlRouterProvider){
   $locationProvider.html5Mode(true);
   $stateProvider
   .state("index",{
@@ -20,21 +19,37 @@ function Router($stateProvider, $locationProvider){
     templateUrl: "/assets/html/meditation-index.html",
     controller: "Index",
     controllerAs: "IndexVm"
-  })
+  });
+  $urlRouterProvider.otherwise("/")
 }
 
 MeditationFactory.$inject = ["$resource"];
 function MeditationFactory($resource){
-  var Meditations = $resource("/api/meditations", {}, {
+  var Meditations = $resource("/api/meditations/:_id", {}, {
     update: {method: "PUT"}
   });
   return Meditations;
 }
 
-IndexCtrl.$inject = ["Meditation", "$stateParams", "$state"];
-function IndexCtrl(Meditation, $stateParams, state){
+IndexCtrl.$inject = ["Meditation"];
+function IndexCtrl(Meditation){
   var vm = this;
   vm.meditations = Meditation.query();
+  vm.create = function(){
+    Meditation.save(vm.newMeditation, function(response){
+      vm.meditations.push(response);
+    })
+  }
+  vm.update = function(meditation, index){
+    Meditation.update({_id: meditation._id}, function(){
+      vm.meditations.push(index, 1)
+    })
+  }
+  vm.destroy = function(meditation, index){
+     Meditation.remove({_id: meditation._id}, function(){
+       vm.meditations.splice(index, 1);
+     });
+   };
 }
 
 })();
